8dbb7eac1ad48fb9481a4f632ce3020fde019d98
TAurelien <aurelien.ticot@gmail.com>
2015-02-26 19:51:20 +0100

    Add pre and post processing for save on Account model

--- src/app/models/account.model.js
+++ src/app/models/account.model.js
@@ -1,5 +1,7 @@
-/** @module Account model */
+/** @module Account Model */
 'use strict';
+
+var logger = require(process.env.LOGGER)('Account Model');
 
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
@@ -7,6 +9,12 @@ var Schema   = mongoose.Schema;
 var constants = require(process.env.CONSTANTS);
 
 var AccountSchema = new Schema({
+
+	generalLedger :{
+		type: Schema.ObjectId,
+		ref: 'GeneralLedger',
+		required : true
+	},
 
 	name: {
 		type: String,
@@ -43,9 +51,18 @@ var AccountSchema = new Schema({
 		enum : constants.commoditiesAsArray
 	},
 
-	balance: {
-		type: Number,
-		default: 0
+	balance : {
+
+		own: {
+			type: Number,
+			default: 0
+		},
+
+		child: {
+			type: Number,
+			default: 0
+		}
+
 	},
 
 	placeholder: {
@@ -63,13 +80,89 @@ var AccountSchema = new Schema({
 		ref: 'Account'
 	},
 
-	// add a setter to determine the level from the parent's one (automatic?)
 	level: {
 		type: Number,
 		default: 0,
 		min : 0
+	},
+
+	meta: {
+		created: Date,
+		creationUser : String,
+		updated: Date,
+		updatedUser : String
 	}
 
 });
+
+// Virtuals ===================================================================
+
+AccountSchema.virtual('balance.total').get(function() {
+
+	return this.balance.own + this.balance.child;
+
+});
+
+// Pre processing methods =====================================================
+
+AccountSchema.pre('save', function(next) {
+
+	logger.debug('====== New Account saving ================================');
+
+	logger.debug('Name : ' + this.name);
+
+	// meta dates management
+
+	var today = new Date();
+
+	if (!this.meta.created) {
+		this.meta.created = today;
+	}
+
+	this.meta.updated = today;
+
+	// child balance management
+
+
+
+	// level management
+
+	var accountId = this._id;
+
+	if (this.parent) {
+		mongoose.model('Account').findById(this.parent, function(err, parent) {
+			if(!err && accountId && parent){
+				var newLevel = parent.level + 1;
+				mongoose.model('Account').findByIdAndUpdate(accountId, { level: newLevel }, { new : true })
+				.exec(function(err, account){
+					logger.debug('updated level : ' + account.level);
+				});
+			}
+		});
+	}
+
+	// ------------------------------------------------------------------------
+
+	logger.debug('');
+
+	next();
+
+});
+
+
+// Post processing methods ====================================================
+
+AccountSchema.post('save', function(account) {
+
+	logger.debug('====== New Account saved =================================');
+	logger.debug('_id : ' + account._id);
+	logger.debug('Name : ' + account.name);
+	logger.debug('total balance : ' + account.balance.total);
+	logger.debug('');
+
+});
+
+
+// Model export ===============================================================
 
 module.exports = mongoose.model('Account', AccountSchema);
--- src/config/env/dev/testingAccountsList.js
+++ src/config/env/dev/testingAccountsList.js
@@ -6,409 +6,493 @@ module.exports = [
 	// Equity =================================================================
 
 	{
+		_id: '54eeeffbc1db710c291be44b',
 		name : 'Equity',
 		description : '',
 		code : '1',
 		type : 'equity',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 2000,
-		aggregatedBalance : 2000,
+		balance : {
+			child : 2000,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
 		parent : null,
-		level : 0
+		level : 0,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 
 	{
+		_id: '54eef4ba4326bb0c13b2c112',
 		name : 'Initial state',
 		description : '',
 		code : '12',
 		type : 'equity',
 		commodity : 'EUR',
-		balance : 2000,
-		childBalance : 0,
-		aggregatedBalance : 2000,
+		balance : {
+			child : 0,
+			own : 2000
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eeeffbc1db710c291be44b',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 
 	// Assets =================================================================
 
 	{
+		_id: '54eef4ba4326bb0c13b2c113',
 		name : 'Assets',
 		description : '',
 		code : '2',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 14300,
-		aggregatedBalance : 14300,
+		balance : {
+			child : 14300,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
 		parent : null,
-		level : 0
+		level : 0,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c114',
 		name : 'Currents',
 		description : '',
 		code : '21',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 5300,
-		aggregatedBalance : 5300,
+		balance : {
+			child : 5300,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c113',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c115',
 		name : 'Cash',
 		description : '',
 		code : '211',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 3300,
-		childBalance : 0,
-		aggregatedBalance : 3300,
+		balance : {
+			child : 0,
+			own : 3300
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c114',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c116',
 		name : 'Savings',
 		description : '',
 		code : '212',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 2000,
-		childBalance : 0,
-		aggregatedBalance : 2000,
+		balance : {
+			child : 0,
+			own : 2000
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c114',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c117',
 		name : 'Long-terms',
 		description : '',
 		code : '22',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 9000,
-		aggregatedBalance : 9000,
+		balance : {
+			child : 9000,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c113',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c118',
 		name : 'Real estate',
 		description : '',
 		code : '221',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 9000,
-		childBalance : 0,
-		aggregatedBalance : 9000,
+		balance : {
+			child : 0,
+			own : 9000
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c117',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c119',
 		name : 'Companies',
 		description : '',
 		code : '222',
 		type : 'asset',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 0,
-		aggregatedBalance : 0,
+		balance : {
+			child : 0,
+			own : 0
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c117',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 
 	// Liabilities ============================================================
 
 	{
+		_id: '54eef4ba4326bb0c13b2c11a',
 		name : 'Liabilities',
 		description : '',
 		code : '4',
 		type : 'liability',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 9300,
-		aggregatedBalance : 9300,
+		balance : {
+			child : 9300,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
 		parent : null,
-		level : 0
+		level : 0,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c11b',
 		name : 'Currents',
 		description : '',
 		code : '41',
 		type : 'liability',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 450,
-		aggregatedBalance : 450,
+		balance : {
+			child : 450,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c11a',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c11c',
 		name : 'Credit cards',
 		description : '',
 		code : '411',
 		type : 'liability',
 		commodity : 'EUR',
-		balance : 400,
-		childBalance : 0,
-		aggregatedBalance : 400,
+		balance : {
+			child : 0,
+			own : 400
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c11b',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c11d',
 		name : 'Invoices',
 		description : '',
 		code : '412',
 		type : 'liability',
 		commodity : 'EUR',
-		balance : 50,
-		childBalance : 0,
-		aggregatedBalance : 50,
+		balance : {
+			child : 0,
+			own : 50
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c11b',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c11e',
 		name : 'Long-terms',
 		description : '',
 		code : '42',
 		type : 'liability',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 8850,
-		aggregatedBalance : 8850,
+		balance : {
+			child : 8850,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c11a',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c11f',
 		name : 'Loans',
 		description : '',
 		code : '421',
 		type : 'liability',
 		commodity : 'EUR',
-		balance : 8850,
-		childBalance : 0,
-		aggregatedBalance : 8850,
+		balance : {
+			child : 0,
+			own : 8850
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c11e',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 
 	// Incomes ================================================================
 
 	{
+		_id: '54eef4ba4326bb0c13b2c120',
 		name : 'Incomes',
 		description : '',
 		code : '7',
 		type : 'income',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 4650,
-		aggregatedBalance : 4650,
+		balance : {
+			child : 4650,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
 		parent : null,
-		level : 0
+		level : 0,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c121',
 		name : 'Recurring',
 		description : '',
 		code : '71',
 		type : 'income',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 3650,
-		aggregatedBalance : 3650,
+		balance : {
+			child : 3650,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c120',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c122',
 		name : 'Salaries',
 		description : '',
 		code : '711',
 		type : 'income',
 		commodity : 'EUR',
-		balance : 3000,
-		childBalance : 0,
-		aggregatedBalance : 3000,
+		balance : {
+			child : 0,
+			own : 300,
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c121',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c123',
 		name : 'Rents',
 		description : '',
 		code : '712',
 		type : 'income',
 		commodity : 'EUR',
-		balance : 650,
-		childBalance : 0,
-		aggregatedBalance : 650,
+		balance : {
+			child : 0,
+			own : 650
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c121',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c124',
 		name : 'Exceptional',
 		description : '',
 		code : '72',
 		type : 'income',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 1000,
-		aggregatedBalance : 1000,
+		balance : {
+			child : 1000,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c120',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c125',
 		name : 'Salaries',
 		description : '',
 		code : '721',
 		type : 'income',
 		commodity : 'EUR',
-		balance : 1000,
-		childBalance : 0,
-		aggregatedBalance : 1000,
+		balance : {
+			child : 0,
+			own : 100,
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c124',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 
 	// Expenses ===============================================================
 
 	{
+		_id: '54eef4ba4326bb0c13b2c126',
 		name : 'Expenses',
 		description : '',
 		code : '6',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 1650,
-		aggregatedBalance : 1650,
+		balance : {
+			child : 1650,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
 		parent : null,
-		level : 0
+		level : 0,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c127',
 		name : 'Recurring',
 		description : '',
 		code : '61',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 1150,
-		aggregatedBalance : 1150,
+		balance : {
+			child : 1150,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c126',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c128',
 		name : 'House',
 		description : '',
 		code : '611',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 800,
-		childBalance : 0,
-		aggregatedBalance : 800,
+		balance : {
+			child : 0,
+			own : 800
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c127',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c129',
 		name : 'Financial fees',
 		description : '',
 		code : '612',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 50,
-		childBalance : 0,
-		aggregatedBalance : 50,
+		balance : {
+			child : 0,
+			own : 50
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c127',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c12a',
 		name : 'Real estate',
 		description : '',
 		code : '613',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 300,
-		childBalance : 0,
-		aggregatedBalance : 300,
+		balance : {
+			child : 0,
+			own : 300
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c127',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c12b',
 		name : 'Exceptional',
 		description : '',
 		code : '62',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 0,
-		childBalance : 500,
-		aggregatedBalance : 500,
+		balance : {
+			child : 500,
+			own : 0
+		},
 		placeholder : true,
 		closed : false,
-		parent : null,
-		level : 1
+		parent : '54eef4ba4326bb0c13b2c126',
+		level : 1,
+		generalLedger : '54ef204e923c579c1967379a'
 	},
 	{
+		_id: '54eef4ba4326bb0c13b2c12c',
 		name : 'Misc',
 		description : '',
 		code : '621',
 		type : 'expense',
 		commodity : 'EUR',
-		balance : 500,
-		childBalance : 0,
-		aggregatedBalance : 500,
+		balance : {
+			child : 0,
+			own : 500
+		},
 		placeholder : false,
 		closed : false,
-		parent : null,
-		level : 2
+		parent : '54eef4ba4326bb0c13b2c12b',
+		level : 2,
+		generalLedger : '54ef204e923c579c1967379a'
 	}
 
 ];
