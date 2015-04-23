51798873b2cae78cb6efaa37599f2606b035e670
TAurelien <aurelien.ticot@gmail.com>
2015-04-23 08:38:05 +0200

    Update gruntfile.js to etter manage setup and restart on change

--- gruntfile.js
+++ gruntfile.js
@@ -1,168 +1,263 @@
 'use strict';
 
-var binDir = 'dist/';
+var _ = require('lodash');
 
-module.exports = function(grunt) {
+var srcDir = 'src';
+var publicDir = 'public';
+var libDir = 'lib';
+var binDir = 'bin';
 
-	// Unified Watch Object
-	var srcFiles = {
-		serverJS: ['src/server.js', 'src/config/**/*.js', 'src/app/**/*.js'],
-		clientViews: ['src/public/**/*.html'],
-		clientJS: ['src/public/assets/js/**/*.js', 'src/public/app/**/*.js'],
-		clientCSS: ['src/public/assets/css/**/*.css']
+var properties = require('./' + binDir + '/properties.json');
+
+module.exports = function (grunt) {
+
+	var binFiles = {
+		serverFiles: [
+			binDir + '/**/*.js',
+			binDir + '/**/*.json',
+			'!' + binDir + '/**/' + publicDir
+		],
+
+		clientViews: [
+			binDir + '/**/' + publicDir + '/**/*.html',
+			'!' + binDir + '/**/' + publicDir + '/**/' + libDir
+		],
+
+		clientJS: [
+			binDir + '/**/' + publicDir + '/**/*.js',
+			'!' + binDir + '/**/' + publicDir + '/**/' + libDir
+		],
+
+		clientCSS: [
+			binDir + '/**/' + publicDir + '/**/*.css',
+			'!' + binDir + '/**/' + publicDir + '/**/' + libDir
+		],
+
+		clientSCSS: [
+			binDir + '/**/' + publicDir + '/**/*.scss',
+			'!' + binDir + '/**/' + publicDir + '/**/' + libDir
+		]
 	};
 
+	var srcFiles = {
 
-	// Project Configuration
+		serverFiles: [
+			srcDir + '/**/*.js',
+			srcDir + '/**/*.json',
+			'!' + srcDir + '/**/' + publicDir
+		],
+
+		clientViews: [
+			srcDir + '/**/' + publicDir + '/**/*.html',
+			'!' + srcDir + '/**/' + publicDir + '/**/' + libDir
+		],
+
+		clientJS: [
+			srcDir + '/**/' + publicDir + '/**/*.js',
+			'!' + srcDir + '/**/' + publicDir + '/**/' + libDir
+		],
+
+		clientCSS: [
+			srcDir + '/**/' + publicDir + '/**/*.css',
+			'!' + srcDir + '/**/' + publicDir + '/**/' + libDir
+		],
+
+		clientSCSS: [
+			srcDir + '/**/' + publicDir + '/**/*.scss',
+			'!' + srcDir + '/**/' + publicDir + '/**/' + libDir
+		]
+
+	};
+
+	var dataCollections = {
+
+		all: [{
+			name: 'currencies',
+			type: 'json',
+			file: 'data/currencies.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}, {
+			name: 'accountTypes',
+			type: 'json',
+			file: 'data/accountTypes.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}],
+
+		dev: [{
+			name: 'generalLedgers',
+			type: 'json',
+			file: 'data/development/generalLedgers.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}, {
+			name: 'accounts',
+			type: 'json',
+			file: 'data/development/accounts.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}, {
+			name: 'transactions',
+			type: 'json',
+			file: 'data/development/transactions.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}],
+
+		test: [{
+			name: 'generalLedgers',
+			type: 'json',
+			file: 'data/test/generalLedgers.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}, {
+			name: 'accounts',
+			type: 'json',
+			file: 'data/test/accounts.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}, {
+			name: 'transactions',
+			type: 'json',
+			file: 'data/test/transactions.json',
+			jsonArray: true,
+			upsert: true,
+			drop: true
+		}]
+
+	}
+
+	// ========================================================================
+
 	grunt.initConfig({
 
 		pkg: grunt.file.readJSON('package.json'),
 
 		watch: {
-			serverJS: {
-				files: srcFiles.serverJS,
-				tasks: ['copy:serverJS'],
+			serverFiles: {
+				files: srcFiles.serverFiles,
+				tasks: ['modif'],
 				options: {
-					livereload: true
+					livereload: false,
+					spawn: false
 				}
 			},
 			clientViews: {
 				files: srcFiles.clientViews,
-				tasks: ['copy:clientViews'],
+				tasks: ['modif'],
 				options: {
 					livereload: true,
+					spawn: false
 				}
 			},
 			clientJS: {
 				files: srcFiles.clientJS,
-				tasks: ['copy:clientJS'],
+				tasks: ['modif'],
 				options: {
-					livereload: true
+					livereload: true,
+					spawn: false
 				}
 			},
 			clientCSS: {
 				files: srcFiles.clientCSS,
-				tasks: ['copy:clientCSS'],
+				tasks: ['modif'],
 				options: {
-					livereload: true
+					livereload: true,
+					spawn: false
+				}
+			},
+			clientSCSS: {
+				files: srcFiles.clientSCSS,
+				tasks: ['modif'],
+				options: {
+					livereload: true,
+					spawn: false
 				}
 			}
 		},
 
-
-		clean:{
-			all:[binDir + '*'],
-			js:[binDir + '**/*.js', '!' + binDir + 'public/assets/libs/**/*/js'],
-			css:[binDir + '**/*.css', '!' + binDir + 'public/assets/libs/**/*/css'],
-			libs:[binDir + 'public/assets/libs/**/*']
-		},
-
-
-		copy:{
-			all:{
-				expand: true,
-				cwd: 'src/',
-				src: ['**'],
-				dest: binDir
-			},
-			serverJS:{
-				expand: true,
-				cwd: 'src/',
-				src: ['server.js', 'config/**/*.js', 'app/**/*.js'],
-				dest: binDir
-			},
-			clientViews:{
-				expand: true,
-				cwd: 'src/',
-				src: ['public/**/*.html'],
-				dest: binDir
-			},
-			clientJS:{
-				expand: true,
-				cwd: 'src/',
-				src: ['public/assets/js/**/*.js', 'public/app/**/*.js'],
-				dest: binDir
-			},
-			clientCSS:{
-				expand: true,
-				cwd: 'src/',
-				src: ['public/assets/css/**/*.css'],
-				dest: binDir
+		sync: {
+			default: {
+				files: [{
+					cwd: srcDir + '/',
+					src: ['**', '!**/*.scss'],
+					dest: binDir + '/',
+				}],
+				pretend: false,
+				verbose: true,
+				updateAndDelete: true
 			}
 		},
 
+		clean: {
+			all: [binDir + '/*']
+		},
+
+		copy: {
+			all: {
+				expand: true,
+				cwd: srcDir + '/',
+				src: ['**', '!**/*.scss'],
+				dest: binDir + '/'
+			}
+		},
 
 		jshint: {
 			all: {
-				src: srcFiles.clientJS.concat(srcFiles.serverJS),
+				src: srcFiles.clientJS.concat(srcFiles.serverFiles),
+				options: {
+					jshintrc: true
+				}
+			},
+			modif: {
+				src: [],
 				options: {
 					jshintrc: true
 				}
 			}
 		},
 
-
-		csslint: {
+		mongoimport: {
 			options: {
-				csslintrc: '.csslintrc',
-			},
-			all: {
-				src: srcFiles.clientCSS
+				db: properties.dev.db.database,
+				host: properties.dev.db.host,
+				port: properties.dev.db.port,
+				stopOnError: false,
+				collections: dataCollections.dev
 			}
 		},
-
-
-		uglify: {
-			production: {
-				options: {
-					mangle: false
-				},
-				files: {
-					'': ''
-				}
-			}
-		},
-
-
-		cssmin: {
-			combine: {
-				files: {
-					'': '<%= applicationCSSFiles %>'
-				}
-			}
-		},
-
 
 		nodemon: {
 			default: {
-				script: binDir + 'server.js',
+				script: binDir + '/server.js',
 				options: {
 					nodeArgs: [''],
-					ext: 'js',
-					watch: srcFiles.serverJS,
-					delay: 5000
+					watch: binFiles.serverFiles
 				}
 			},
 			debug: {
-				script: binDir + 'server.js',
+				script: binDir + '/server.js',
 				options: {
 					nodeArgs: ['--debug'],
-					ext: 'js',
-					watch: srcFiles.serverJS,
-					delay: 5000
+					watch: binFiles.serverFiles
 				}
 			},
 			debugbrk: {
-				script: binDir + 'server.js',
+				script: binDir + '/server.js',
 				options: {
 					nodeArgs: ['--debug-brk'],
-					ext: 'js',
-					watch: srcFiles.serverJS,
-					delay: 5000
+					watch: binFiles.serverFiles
 				}
 			}
 		},
-
 
 		'node-inspector': {
 			custom: {
@@ -178,7 +273,6 @@ module.exports = function(grunt) {
 			}
 		},
 
-
 		concurrent: {
 			default: ['nodemon:default', 'watch'],
 			debug: ['nodemon:debug', 'watch', 'node-inspector'],
@@ -189,8 +283,10 @@ module.exports = function(grunt) {
 			}
 		},
 
-
 		env: {
+			prod: {
+				NODE_ENV: 'production'
+			},
 			dev: {
 				NODE_ENV: 'development'
 			},
@@ -199,7 +295,34 @@ module.exports = function(grunt) {
 			}
 		}
 
+	});
 
+	var watchedEvents = Object.create(null);
+
+	var onChange = _.debounce(function () {
+
+		var events = watchedEvents;
+		// { path : action }
+		// path => 'src\\server.js' file or folder
+		// action => 'changed', 'added', 'deleted', 'renamed'
+
+		var jshintArray = [];
+
+		_.forEach(events, function (action, path) {
+			if (action !== 'deleted') {
+				jshintArray.push(path);
+			}
+		});
+
+		grunt.config('jshint.modif.src', jshintArray);
+
+		watchedEvents = Object.create(null);
+
+	}, 200);
+
+	grunt.event.on('watch', function (action, filepath) {
+		watchedEvents[filepath] = action;
+		onChange();
 	});
 
 	// Load NPM tasks
@@ -211,22 +334,23 @@ module.exports = function(grunt) {
 	// A Task for loading the configuration object
 	// Check Yo project
 
-	// Lint task(s).
-	grunt.registerTask('lint', ['jshint', 'csslint']);
+	// Initialization tasks
+	grunt.registerTask('lint', ['jshint:all']);
+	grunt.registerTask('init', ['lint', 'sync:default']);
+	grunt.registerTask('modif', ['jshint:modif', 'sync:default']);
+	grunt.registerTask('setup_dev_db', ['mongoimport'])
 
-	// Initialization task(s).
-	grunt.registerTask('init', ['lint', 'clean:all', 'copy:all']);
+	// Starting tasks
+	grunt.registerTask('default', ['env:dev', 'setup_dev_db', 'init', 'concurrent:default']);
+	grunt.registerTask('debug', ['env:dev', 'setup_dev_db', 'init', 'concurrent:debug']);
+	grunt.registerTask('debugbrk', ['env:dev', 'setup_dev_db', 'init', 'concurrent:debugbrk']);
 
-	// Default task(s).
-	grunt.registerTask('default', ['env:dev', 'init', 'concurrent:default']);
+	// Testing tasks
+	// TODO Define grunt test tasks
+	//grunt.registerTask('test', ['env:test', 'lint']);
 
-	// Debug task.
-	grunt.registerTask('debug', ['env:dev', 'init', 'concurrent:debug']);
-
-	// Debug task, start paused.
-	grunt.registerTask('debugbrk', ['env:dev', 'init', 'concurrent:debugbrk']);
-
-	// Build task(s).
-	grunt.registerTask('build', ['lint', 'uglify', 'cssmin']);
+	// Build / Release tasks
+	// TODO Define grunt build tasks
+	//grunt.registerTask('test', ['env:prod', 'setup_test_db', 'lint']);
 
 };
