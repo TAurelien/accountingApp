d40622da1fbac00de68210ca06ddb7f86b027967
TAurelien <aurelien.ticot@gmail.com>
2015-05-01 00:58:09 +0200

    Create component module structures

---
+++ src/components/frontend/src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function setup(options, imports, register) {
+	console.log('Setting up a component module ...');
+
+	// Register --------------
+
+	register(null);
+
+};
