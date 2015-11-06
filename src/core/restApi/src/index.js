022253719cd6db8a78ca8b741a6c3bba1dc10cd9
TAurelien <aurelien.ticot@gmail.com>
2015-11-06 22:55:42 +0100

    Implement REST API module

---
+++ src/core/restApi/src/index.js
@@ -0,0 +1,43 @@
+/**
+ * @module REST API
+ */
+'use strict';
+
+module.exports = function setup(options, imports, register) {
+	console.log();
+	console.log('Registering the REST API core module ...');
+
+	var logger = imports.logger.get('REST API');
+
+	var app = imports.express.app;
+	var express = imports.express.express;
+
+	var apiRouter = express.Router();
+
+	apiRouter.use(function (req, res, next) {
+		next();
+	});
+
+	var addRoute = function (route, callback) {
+		logger.debug('Defining a new route for', route);
+		// TODO Deal with missing /
+		var newRoute = apiRouter.route(route);
+		callback(newRoute);
+	};
+
+	var registerAPIRoutes = function () {
+		logger.info('Registering REST API routes');
+		app.use('/api', apiRouter);
+	};
+
+	// Register --------------
+
+	register(null, {
+
+		restApi: {
+			addRoute: addRoute,
+			preDBConnectionConfig: registerAPIRoutes
+		}
+
+	});
+};
