d7bedcb13737ef41e859f598c604cd26b0ab27b4
TAurelien <aurelien.ticot@gmail.com>
2015-11-15 14:08:00 +0100

    Add api argument in exported function of events.js

--- src/components/transactions/src/controller/events.js
+++ src/components/transactions/src/controller/events.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function (options, imports) {
+module.exports = function (options, imports, api) {
 
 	var logger = imports.logger.get('General Ledgers ctrl - Events');
 
