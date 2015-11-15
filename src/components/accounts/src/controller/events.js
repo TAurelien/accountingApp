27457fc20a1fcd4f0d923934b079edea0fff0bb6
TAurelien <aurelien.ticot@gmail.com>
2015-11-15 14:53:04 +0100

    handle transactions changes for account balance update

--- src/components/accounts/src/controller/events.js
+++ src/components/accounts/src/controller/events.js
@@ -1,26 +1,55 @@
 'use strict';
 
-module.exports = function (options, imports) {
+var _ = require('lodash');
+var async = require('async');
+
+module.exports = function (options, imports, api) {
 
 	var logger = imports.logger.get('Accounts ctrl - Events');
 	var Transactions = imports.transactions;
 
+	var handleTransaction = function (transaction) {
+		var accounts = [];
+		if (transaction && transaction.splits) {
+			_.forEach(transaction.splits, function (split) {
+				var accountID = split.account.toString();
+				if (_.indexOf(accounts, accountID) < 0) {
+					accounts.push(accountID);
+				}
+			});
+		}
+		logger.debug('Async update of accounts', accounts); // TODO Remove the logger.debug
+		async.each(
+			accounts,
+			function (accountID, asyncCallback) {
+				logger.debug('Updating the balance of the account', accountID); // TODO Remove the logger.debug
+				api.updateBalance(accountID, function (err) {
+					asyncCallback(err);
+				});
+			},
+			function (err) {
+				if (err) {
+					logger.error('The update of all account balances impacted by the transaction has failed');
+				} else {
+					logger.info('All account balances impacted by the transaction have been updated accordingly');
+				}
+			}
+		);
+	};
+
 	Transactions.on(Transactions.events.CREATED, function (createdItem) {
 		logger.info('Handling a', Transactions.events.CREATED, 'event');
-		logger.debug('A transaction has been created, I should update the corresponding accounts');
-		logger.debug(createdItem.id);
+		handleTransaction(createdItem);
 	});
 
 	Transactions.on(Transactions.events.UPDATED, function (updatedItem) {
 		logger.info('Handling a', Transactions.events.UPDATED, 'event');
-		logger.debug('A transaction has been updated, I should update the corresponding accounts');
-		logger.debug(updatedItem.id);
+		handleTransaction(updatedItem);
 	});
 
 	Transactions.on(Transactions.events.DELETED, function (deletedItem) {
 		logger.info('Handling a', Transactions.events.DELETED, 'event');
-		logger.debug('A transaction has been deleted, I should update the corresponding accounts');
-		logger.debug(deletedItem.id);
+		handleTransaction(deletedItem);
 	});
 
 };
