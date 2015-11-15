8014229275c1cc05286e3944db333fafb203f9a4
TAurelien <aurelien.ticot@gmail.com>
2015-11-15 14:53:39 +0100

    handle account changes for general ledger net worth update

--- src/components/generalLedgers/src/controller/events.js
+++ src/components/generalLedgers/src/controller/events.js
@@ -1,32 +1,42 @@
 'use strict';
 
-module.exports = function (options, imports) {
+module.exports = function (options, imports, api) {
 
 	var logger = imports.logger.get('General Ledgers ctrl - Events');
 	var Accounts = imports.accounts;
 
+	var handleAccount = function (account) {
+		var accountId = account.id;
+		if (account.generalLedger && account.generalLedger.toString) {
+			var generalLedgerId = account.generalLedger.toString();
+			api.updateNetWorth(generalLedgerId, function (err) {
+				if (err) {
+					logger.error('The update of net worth of the general ledger (', generalLedgerId, ') impacted by the account(', accountId, ') has failed');
+				} else {
+					logger.info('The net worth of the general ledger (', generalLedgerId, ') impacted by the account(', accountId, ') has been successfully updated');
+				}
+			});
+		}
+	};
+
 	Accounts.on(Accounts.events.CREATED, function (createdItem) {
 		logger.info('Handling a', Accounts.events.CREATED, 'event');
-		logger.debug('An account has been created, I should update the corresponding general ledger');
-		logger.debug(createdItem.id);
+		handleAccount(createdItem);
 	});
 
 	Accounts.on(Accounts.events.UPDATED, function (updatedItem) {
 		logger.info('Handling a', Accounts.events.UPDATED, 'event');
-		logger.debug('An account has been updated, I should update the corresponding general ledger');
-		logger.debug(updatedItem.id);
+		handleAccount(updatedItem);
 	});
 
 	Accounts.on(Accounts.events.DELETED, function (deletedItem) {
 		logger.info('Handling a', Accounts.events.DELETED, 'event');
-		logger.debug('An account has been deleted, I should update the corresponding general ledger');
-		logger.debug(deletedItem);
+		handleAccount(deletedItem);
 	});
 
 	Accounts.on(Accounts.events.BALANCE_CHANGED, function (item) {
 		logger.info('Handling a', Accounts.events.BALANCE_CHANGED, 'event');
-		logger.debug('The balance of an account has been changed, I should update the corresponding general ledger');
-		logger.debug(item.id);
+		handleAccount(item);
 	});
 
 };
